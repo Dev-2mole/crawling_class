c4b1f06823051204236024cbcc50f86ce2789739
Dev-2mole <2mole@kakao.com>
2023-11-19 23:28:11 +0900

    수정중

--- source/bungaganto.py
+++ source/bungaganto.py
@@ -1,2 +1,79 @@
 # 이 페이지는 번개장터 페이지에서 데이터를 받아올 소스코드입니다.
 # 페이지 결과는 ../data/air 폴더에 저장될 예정입니다.
+
+import random
+from time import sleep
+# from datetime import *
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+class InfoCrawler():
+    def __init__(self):
+        self.base_url = ""
+        self.headers = {}
+        self.user_agent_list = [
+            #Chrome
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+
+            #Firefox
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/96.0'
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/95.0',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/94.0',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/93.0',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/92.0',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:96.0) Gecko/20100101 Firefox/91.0'
+        ]
+
+    def set_random_user_agent(self):
+        user_agent = random.choice(self.user_agent_list)
+        self.headers['User-Agent'] = user_agent
+        return user_agent
+
+class bungaganto_Crawler(InfoCrawler):
+
+    def __init__(self):
+        super().__init__()
+        self.base_url = "https://m.bunjang.co.kr/search/products?q=32QN650"
+        self.headers = {
+            'User-Agent': self.set_random_user_agent(),
+            'referer': "https://m.bunjang.co.kr/",
+            'Cache-Control': 'no-cache',
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+        self.get_code()
+
+    def get_code(self):
+        options = ChromeOptions()
+        options.add_argument(f"user-agent={self.headers['User-Agent']}")  # 수정된 부분
+        options.add_argument("lang=ko_KR")
+        # headless 모드 비활성화
+        # options.add_argument('headless')
+        options.add_argument("start-maximized")
+        options.add_argument("disable-gpu")
+        options.add_argument("--no-sandbox")
+
+        # 크롬 드라이버 최신 버전 설정
+        service = ChromeService(executable_path=ChromeDriverManager().install())
+
+        # chrome driver
+        self.driver = webdriver.Chrome(service=service, options=options)
+
+        # 번개장터 웹 페이지 열기
+        self.driver.get(self.base_url)
+
+
+        sleep(10)
+        # 웹 드라이버 종료
+        self.driver.quit()
+
+
+
+crawler = bungaganto_Crawler()

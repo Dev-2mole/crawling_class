855ec2110cca3695a184b4cb9b7bdbb098d477a3
Dev-2mole <2mole@kakao.com>
2023-12-11 02:43:36 +0900

    finish data.py

--- source/data.py
+++ source/data.py
@@ -1,54 +1,67 @@
 # 이 페이지는 data 파일을 가져와서 데이터 비교를 진행할 예정입니다. 항공 data vs 통계청 data
 # 결과물은 main.py 를 이용하여 전달(표현)할 예정입니다.
+# 영어 주석 연습 중입니다.
+
 import os
+import platform
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import rc
 
 def main():
-        
-    download_path = os.path.join(os.getcwd(), 'data', 'air')
-    result_path =  os.path.join(os.getcwd(), 'data', 'air','result')
+    downloaded_path = os.path.join(os.getcwd(), 'data', 'air')                  # Downloaded Data Location
+    result_path =  os.path.join(os.getcwd(), 'data', 'air','result')            # Result Data Location
+    # Check Folder Location
     if not os.path.exists(result_path):
                 os.makedirs(result_path)
-    # 'AppleGothic' 폰트를 matplotlib의 기본 폰트로 설정
-    rc('font', family='AppleGothic')
-    plt.rcParams['axes.unicode_minus'] = False
+    
+    # Different OS Font Settings
+    os_platform = platform.system()         # Check OS
+    # Mac OS : AppleGothic.ttf
+    if os_platform == "Darwin":
+        plt.rcParams['font.family'] = 'AppleGothic'
+        plt.rcParams['axes.unicode_minus'] = False
+    # Windows 10, 11 : Gulim.ttf
+    elif os_platform == "Windows" and platform.release() in ["10", "11"]:
+        plt.rcParams['font.family'] = 'Gulim'
+        plt.rcParams['axes.unicode_minus'] = False
+ 
+    # Path List Of Files
+    file_paths = [downloaded_path+f'/{year}.xlsx' for year in range(2017, 2023)]
 
-    # 파일 경로 리스트 생성
-    file_paths = [download_path+f'/{year}.xlsx' for year in range(2017, 2023)]
-
-    # 각 파일을 불러와 데이터 프레임 생성
+    # Create DataFrame By Each Files
     dfs = [pd.read_excel(path) for path in file_paths]
 
-    # '여객 출발' 및 '여객 도착' 컬럼을 숫자형으로 변환
+    # Convert '여객 출발' 및 '여객 도착' Columns To Integer
     for df in dfs:
         df['여객 출발'] = pd.to_numeric(df['여객 출발'], errors='coerce').fillna(0)
         df['여객 도착'] = pd.to_numeric(df['여객 도착'], errors='coerce').fillna(0)
 
-    # 년도별 상위 5개 국가의 여객 출발 및 도착 시각화 및 저장
+    # Passenger Departures And Arrivals In The Top 5 Countries By Year
     for year in range(2017, 2023):
         annual_top5_departures = dfs[year - 2017].groupby('국가 국가')['여객 출발'].sum().sort_values(ascending=False).drop(index='합 계').head(5)
         annual_top5_arrivals = dfs[year - 2017].groupby('국가 국가')['여객 도착'].sum().sort_values(ascending=False).drop(index='합 계').head(5)
 
-        # 그래프 생성
+        # Make Graph
         fig, axes = plt.subplots(1, 2, figsize=(15, 6))
 
-        # 여객 출발
+        # Passenger Departure In Year Data 
         axes[0].bar(annual_top5_departures.index, annual_top5_departures.values, color='blue')
         axes[0].set_title(f'{year}년 여객 출발')
         axes[0].set_ylabel('여객 수')
         axes[0].set_xlabel('국가')
 
-        # 여객 도착
+        # Passengers Arrival In Year Data
         axes[1].bar(annual_top5_arrivals.index, annual_top5_arrivals.values, color='green')
         axes[1].set_title(f'{year}년 여객 도착')
         axes[1].set_ylabel('여객 수')
         axes[1].set_xlabel('국가')
 
-        # 이미지 파일로 저장
-        image_name = result_path+f'{year}_top5_air_traffic.png'
+        # Save Graph to png
+        image_name = result_path+"/"+f'{year}_top5_air_traffic.png'
         plt.tight_layout()
         plt.savefig(image_name)
         plt.close()
 
+if __name__ == "__main__":
+    main()
